c072d13a9b1299612aa8706881038635cd741189
hariharan K <iamhari1997@gmail.com>
2022-06-28 02:38:51 +0530

    search and del added

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/model/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -6,12 +8,27 @@ import { ProductService } from 'src/app/services/product.service';
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css'],
 })
-export class AdminProductsComponent implements OnInit {
-  product$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: Product[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.product$ = this.productService.getAll();
+    this.subscription = this.productService
+      .getAll()
+      .subscribe(
+        (products) => (this.filteredProducts = this.products = products)
+      );
   }
-
+  filter(query: string) {
+    this.filteredProducts = query
+      ? this.products.filter((p) =>
+          p.title.toLowerCase().includes(query.toLowerCase())
+        )
+      : this.products;
+  }
   ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { map } from 'rxjs/operators';
+import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +14,16 @@ export class ProductService {
   }
 
   getAll() {
-    return this.db.list('/products').snapshotChanges();
+    return this.db
+      .list('/products')
+      .snapshotChanges()
+      .pipe(
+        map((actions) =>
+          actions.map(
+            (a) => ({ key: a.key, ...(a.payload.val() as Product) } as Product)
+          )
+        )
+      );
   }
 
   get(productId) {
